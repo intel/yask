19f5a94a5ffdb7c7569eee3261a2e680b478a792
Chuck Yount <chuck.yount@intel.com>
2017-06-30 13:20:36 -0700

    Add license and contributor info.

--- src/coefficients/fd_coeff.cpp
+++ src/coefficients/fd_coeff.cpp
@@ -1,3 +1,31 @@
+/*****************************************************************************
+
+YASK: Yet Another Stencil Kernel
+Copyright (c) 2014-2017, Intel Corporation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to
+deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+* The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+IN THE SOFTWARE.
+
+*****************************************************************************/
+
+// Finite-differences coefficients code.
+// Contributed by Jeremy Tillay.
+
 #include <iostream>
 #include <cstring>
 #include "fd_coeff.hpp"
@@ -19,46 +47,39 @@ f^(m)[eval_point] ~~ sum of coeff[i]*f[point[i]] from i = 0 to num_points-1
 	
 void fd_coeff(float coeff[], float eval_point, int order, float points[], int num_points)
 {
+    float c1, c2, c3;
+    float x_0=eval_point;
+    float center=0;
 
+    //need a vector which stores fd_coefficients
+    //coefficients are in d[end][end][i]
+    float d[order+1][num_points][num_points];
+    memset(d, 0, sizeof(d));
+    d[0][0][0]=1;
+    c1 = 1.f;
 
-
-float c1, c2, c3;
-float x_0=eval_point;
-float center=0;
-
-
-
-
-//need a vector which stores fd_coefficients
-//coefficients are in d[end][end][i]
-float d[order+1][num_points][num_points];
-memset(d, 0, sizeof(d));
-d[0][0][0]=1;
-c1 = 1.f;
-
-
-for(int n=1; n<=num_points-1;n++){
-c2=1.f;
+    for(int n=1; n<=num_points-1;n++){
+        c2=1.f;
 	for(int v=0; v<=n-1; v++){
-	c3 = points[n] - points[v];
-	c2 = c2*c3;
-		for(int m=0; m<=MIN(n, order); ++m){
+            c3 = points[n] - points[v];
+            c2 = c2*c3;
+            for(int m=0; m<=MIN(n, order); ++m){
 		d[m][n][v] = (points[n]-x_0)*d[m][n-1][v] - m*d[m-1][n-1][v];
 		d[m][n][v] *= 1.f/c3;
-		}
+            }
 	}
 	for(int m=0; m<= MIN(n, order); m++){
-	d[m][n][n] = m*d[m-1][n-1][n-1] - (points[n-1]-x_0)*d[m][n-1][n-1];
-	d[m][n][n] *= c1/c2;
+            d[m][n][n] = m*d[m-1][n-1][n-1] - (points[n-1]-x_0)*d[m][n-1][n-1];
+            d[m][n][n] *= c1/c2;
 	}
-c1=c2;
-}
+        c1=c2;
+    }
 
-cout << "DONE COMPUTING COEFFICIENTS"<< endl;
+    cout << "DONE COMPUTING COEFFICIENTS"<< endl;
 
-for(int i=0; i<num_points; i++){
-coeff[i] = d[order][num_points-1][i];
+    for(int i=0; i<num_points; i++){
+        coeff[i] = d[order][num_points-1][i];
 
-}
+    }
 
 }
--- src/coefficients/fd_coeff_test.cpp
+++ src/coefficients/fd_coeff_test.cpp
@@ -1,3 +1,31 @@
+/*****************************************************************************
+
+YASK: Yet Another Stencil Kernel
+Copyright (c) 2014-2017, Intel Corporation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to
+deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+* The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+IN THE SOFTWARE.
+
+*****************************************************************************/
+
+// Finite-differences coefficients code.
+// Contributed by Jeremy Tillay.
+
 #include <iostream>
 #include <cstring>
 #include "fd_coeff.hpp"
@@ -8,43 +36,40 @@ using namespace std;
 
 int main()
 {
+    //set the order of the derivative to approximate
+    //e.g. we want to approximate d^m/dx^m 
+    const int order = 2;
 
-//set the order of the derivative to approximate
-//e.g. we want to approximate d^m/dx^m 
-const int order = 2;
+    //set the evaluation point e.g. we want to approximate some derivative f^(m)[eval_point]
+    //for most application, this is 0
+    float eval_point = 0;
 
-//set the evaluation point e.g. we want to approximate some derivative f^(m)[eval_point]
-//for most application, this is 0
-float eval_point = 0;
+    //Construct a set of points (-h*radius, -h*(radius-1), .. 0, h,..., h*radius) 
+    //Could pass any arbitrary array grid_points = {x_0, x_1, ... x_n} 
+    const int radius = 2;
+    const int num_points = 2*radius+1;
+    float h = 1;
+    float coeff[num_points];
+    float grid_points[num_points];
 
+    for(int i=0; i<num_points; i++){
+        grid_points[i] = (i-(num_points-1)/2)*h;
+        cout << grid_points[i]<<endl;
+    }
 
-//Construct a set of points (-h*radius, -h*(radius-1), .. 0, h,..., h*radius) 
-//Could pass any arbitrary array grid_points = {x_0, x_1, ... x_n} 
-const int radius = 1;
-const int num_points = 2*radius+1;
-float h = 1;
-float coeff[num_points];
-float grid_points[num_points]; 
+    fd_coeff(coeff, eval_point, order, grid_points, num_points);
 
-for(int i=0; i<num_points; i++){
-grid_points[i] = (i-(num_points-1)/2)*h;
-cout << grid_points[i]<<endl;
-}
+    string suffix = (order == 1) ? "st" : (order == 2) ? "nd" : (order == 3) ? "rd" : "th";
+    cout << "The " << order << suffix << " derivative of f("<< eval_point <<
+        ") is approximated by this " << num_points << "-point FD formula:" << endl;
+    cout << "f^(" << order << ")(" << eval_point << ") ~= ";
 
+    for(int i=0; i<num_points; i++) {
+        if (i)
+            cout << " + ";
+        cout << coeff[i] << "*f[" << grid_points[i] << "]";
+    }
+    cout << endl;
 
-fd_coeff(coeff, eval_point, order, grid_points, num_points);
-
-
-//TODO: this is hideous, use printf
-cout << "The " << order << "^th" << "derivative of f("<< eval_point << ") is: "<<endl;
-
-cout << "f^(" << order << ")@ " << eval_point << "~~ " << coeff[0] << "*f[" << grid_points[0] << "]";
-
-for(int i=1; i<num_points-1; i++){
-cout << "+ " << coeff[i] << "*f[" << grid_points[i] << "]";
-}
-
-cout << "+" << coeff[num_points-1] << "*f[" << grid_points[num_points-1] << "]" << endl;
-
-return 0;
+    return 0;
 }
