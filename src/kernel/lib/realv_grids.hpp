intel/yask

commit b873ffb10b4f3ad40ba61ab4aba1020e58bdac5f

Some perf improvement for arbitrary domain sizes, issue #53.
Remainder code only now done with scalar calls.
Consolidated step-index modulo operation across points in a stencil.

src/kernel/Makefile
@@ -266,7 +266,7 @@ NDIMS_OPT		:=	`cat $(YK_DIMS_FILE)`
 RANK_LOOP_OPTS		?=	$(NDIMS_OPT) -inVar rank_idxs
 RANK_LOOP_ORDER		?=	1 .. N-1
 RANK_LOOP_CODE		?=	$(RANK_LOOP_OUTER_MODS) loop($(RANK_LOOP_ORDER)) \
-				{ $(RANK_LOOP_INNER_MODS) call(calc_region(eqGroup_ptr)); }
+				{ $(RANK_LOOP_INNER_MODS) call(calc_region(stGroup_ptr)); }
 
 # Region loops break up a region using OpenMP threading into blocks.  The
 # 'omp' modifier creates an outer OpenMP loop so that each block is assigned
@@ -275,7 +275,7 @@ RANK_LOOP_CODE		?=	$(RANK_LOOP_OUTER_MODS) loop($(RANK_LOOP_ORDER)) \
 # may be found in StencilEquations::calc_region().
 REGION_LOOP_OPTS	?=     	$(NDIMS_OPT) -inVar region_idxs \
 				-ompConstruct '$(omp_par_for) schedule($(omp_region_schedule)) proc_bind(spread)' \
-				-callPrefix 'eg->'
+				-callPrefix 'sg->'
 REGION_LOOP_OUTER_MODS	?=	grouped omp
 REGION_LOOP_ORDER	?=	1 .. N-1
 REGION_LOOP_CODE	?=	$(REGION_LOOP_OUTER_MODS) loop($(REGION_LOOP_ORDER)) { \
@@ -468,8 +468,8 @@ endif
 # VTUNE settings.
 ifeq ($(vtune),1)
  MACROS		+=	USE_VTUNE
-ifneq ($(VTUNE_AMPLIFIER_XE_2018_DIR),)
- VTUNE_DIR	=	$(VTUNE_AMPLIFIER_XE_2018_DIR)
+ifneq ($(VTUNE_AMPLIFIER_2018_DIR),)
+ VTUNE_DIR	=	$(VTUNE_AMPLIFIER_2018_DIR)
 else ifneq ($(VTUNE_AMPLIFIER_XE_2017_DIR),)
  VTUNE_DIR	=	$(VTUNE_AMPLIFIER_XE_2017_DIR)
 else

commit 52e3ed64f487289873e90475d8ecf15f6b03da42

Fix bug in scalar code introduced in b873ffb.
Add more test stencils.

src/kernel/Makefile
@@ -695,13 +695,16 @@ yk-test-no-yc: kernel-only
 # For testing both the kernel and compiler in various combinations,
 # run the tests from the top-level Makefile.
 all-tests:
-	$(MAKE) clean; $(MAKE) stencil=3axis fold=x=4,y=2 cxx-yk-grid-test
-	$(MAKE) clean; $(MAKE) stencil=9axis fold=z=2 cxx-yk-grid-test
-	$(MAKE) clean; $(MAKE) stencil=3plane fold=y=2,z=4 cxx-yk-grid-test
-	$(MAKE) clean; $(MAKE) stencil=cube fold=x=2,y=2,z=2 cxx-yk-grid-test
+	$(MAKE) clean; $(MAKE) stencil=test_3d fold=x=4,y=2 cxx-yk-grid-test
+	$(MAKE) clean; $(MAKE) stencil=test_1d yc-and-yk-test
+	$(MAKE) clean; $(MAKE) stencil=3axis fold=x=4,y=2 yc-and-yk-test
+	$(MAKE) clean; $(MAKE) stencil=9axis fold=z=2 yc-and-yk-test
+	$(MAKE) clean; $(MAKE) stencil=3plane fold=y=2,z=4 yc-and-yk-test
+	$(MAKE) clean; $(MAKE) stencil=cube fold=x=2,y=2,z=2 yc-and-yk-test
+	$(MAKE) clean; $(MAKE) stencil=test_4d fold=w=2,x=2,y=2,z=2 yc-and-yk-test
 	$(MAKE) clean; $(MAKE) stencil=iso3dfd fold=x=4,y=2 yc-and-yk-test
-	$(MAKE) clean; $(MAKE) stencil=awp_elastic yc-and-yk-test real_bytes=8
-	$(MAKE) clean; $(MAKE) stencil=fsg_abc yc-and-yk-test real_bytes=8
+	$(MAKE) clean; $(MAKE) stencil=awp_elastic real_bytes=8 yc-and-yk-test
+	$(MAKE) clean; $(MAKE) stencil=fsg_abc real_bytes=8 yc-and-yk-test
 	$(MAKE) clean; $(MAKE) stencil=iso3dfd cxx-yk-api-test
 	$(MAKE) clean; $(MAKE) stencil=iso3dfd py-yk-api-test
 

src/kernel/lib/realv_grids.hpp
@@ -741,7 +741,7 @@ namespace yask {
                                    bool checkBounds=true) final {
 
             const real_t* p =
-                const_cast<const YkVecGrid*>(this)->getElemPtr(idxs, checkBounds);
+                const_cast<const YkVecGrid*>(this)->getElemPtr(idxs, alloc_step_idx, checkBounds);
             return const_cast<real_t*>(p);
         }
 

src/kernel/lib/settings.cpp
@@ -209,13 +209,11 @@ namespace yask {
             }
         }
 
-        // Option for all domain dims.
-        if (multi_vars.size() > 1) {
-            parser.add_option(new CommandLineParser::MultiIdxOption
-                              (prefix,
-                               "Shorthand for" + multi_help,
-                               multi_vars));
-        }
+        // Option for setting all domain dims.
+        parser.add_option(new CommandLineParser::MultiIdxOption
+                          (prefix,
+                           "Shorthand for" + multi_help,
+                           multi_vars));
     }
     
     // Add these settigns to a cmd-line parser.

src/stencils/SimpleTestStencils.hpp
@@ -1,0 +1,217 @@
+/*****************************************************************************
+
+YASK: Yet Another Stencil Kernel
+Copyright (c) 2014-2017, Intel Corporation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to
+deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+* The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+IN THE SOFTWARE.
+
+*****************************************************************************/
+
+// Some very simple tests.
+
+#include "Soln.hpp"
+
+// Simple tests to increment values in N spatial dims.
+
+class Test1dStencil : public StencilBase {
+
+protected:
+
+    // Indices & dimensions.
+    MAKE_STEP_INDEX(t);           // step in time dim.
+    MAKE_DOMAIN_INDEX(x);         // spatial dim.
+
+    // Vars.
+    MAKE_GRID(data, t, x); // time-varying grid.
+    
+public:
+
+    Test1dStencil(StencilList& stencils) :
+        StencilBase("test_1d", stencils) { }
+    virtual ~Test1dStencil() { }
+
+    // Define equation to apply to all points in 'data' grid.
+    virtual void define() {
+
+        // define the value at t+1.
+        data(t+1, x) EQUALS data(t, x) + 1.0;
+    }
+};
+
+REGISTER_STENCIL(Test1dStencil);
+
+class Test2dStencil : public StencilBase {
+
+protected:
+
+    // Indices & dimensions.
+    MAKE_STEP_INDEX(t);           // step in time dim.
+    MAKE_DOMAIN_INDEX(x);         // spatial dim.
+    MAKE_DOMAIN_INDEX(y);         // spatial dim.
+
+    // Vars.
+    MAKE_GRID(data, t, x, y); // time-varying grid.
+    
+public:
+
+    Test2dStencil(StencilList& stencils) :
+        StencilBase("test_2d", stencils) { }
+    virtual ~Test2dStencil() { }
+
+    // Define equation to apply to all points in 'data' grid.
+    virtual void define() {
+
+        // define the value at t+1.
+        data(t+1, x, y) EQUALS data(t, x, y) + 1.0;
+    }
+};
+
+REGISTER_STENCIL(Test2dStencil);
+
+class Test3dStencil : public StencilBase {
+
+protected:
+
+    // Indices & dimensions.
+    MAKE_STEP_INDEX(t);           // step in time dim.
+    MAKE_DOMAIN_INDEX(x);         // spatial dim.
+    MAKE_DOMAIN_INDEX(y);         // spatial dim.
+    MAKE_DOMAIN_INDEX(z);         // spatial dim.
+
+    // Vars.
+    MAKE_GRID(data, t, x, y, z); // time-varying grid.
+    
+public:
+
+    Test3dStencil(StencilList& stencils) :
+        StencilBase("test_3d", stencils) { }
+    virtual ~Test3dStencil() { }
+
+    // Define equation to apply to all points in 'data' grid.
+    virtual void define() {
+
+        // define the value at t+1.
+        data(t+1, x, y, z) EQUALS data(t, x, y, z) + 1.0;
+    }
+};
+
+REGISTER_STENCIL(Test3dStencil);
+
+class Test4dStencil : public StencilBase {
+
+protected:
+
+    // Indices & dimensions.
+    MAKE_STEP_INDEX(t);           // step in time dim.
+    MAKE_DOMAIN_INDEX(w);         // spatial dim.
+    MAKE_DOMAIN_INDEX(x);         // spatial dim.
+    MAKE_DOMAIN_INDEX(y);         // spatial dim.
+    MAKE_DOMAIN_INDEX(z);         // spatial dim.
+
+    // Vars.
+    MAKE_GRID(data, t, w, x, y, z); // time-varying grid.
+    
+public:
+
+    Test4dStencil(StencilList& stencils) :
+        StencilBase("test_4d", stencils) { }
+    virtual ~Test4dStencil() { }
+
+    // Define equation to apply to all points in 'data' grid.
+    virtual void define() {
+
+        // define the value at t+1.
+        data(t+1, w, x, y, z) EQUALS data(t, w, x, y, z) + 1.0;
+    }
+};
+
+REGISTER_STENCIL(Test4dStencil);
+
+
+// A "stream-like" stencil that just reads and writes
+// with no spatial offsets.
+// The radius controls how many reads are done in the time domain.
+// Running with radius=2 should give performance comparable to
+// (but not identical to) the stream 'triad' benchmark.
+
+class StreamStencil : public StencilRadiusBase {
+
+protected:
+
+    // Indices & dimensions.
+    MAKE_STEP_INDEX(t);           // step in time dim.
+    MAKE_DOMAIN_INDEX(x);         // spatial dim.
+    MAKE_DOMAIN_INDEX(y);         // spatial dim.
+    MAKE_DOMAIN_INDEX(z);         // spatial dim.
+
+    // Vars.
+    MAKE_GRID(data, t, x, y, z); // time-varying 3D grid.
+    
+public:
+
+    StreamStencil(StencilList& stencils, int radius=2) :
+        StencilRadiusBase("test_stream", stencils, radius) { }
+    virtual ~StreamStencil() { }
+
+    // Define equation to read '_radius' values and write one.
+    virtual void define() {
+
+        GridValue v = constNum(1.0);
+
+        // Add '_radius' values from past time-steps to ensure no spatial locality.
+        for (int r = 0; r < _radius; r++) {
+            v += data(t-r, x, y, z);
+        }
+
+        // define the value at t+1 to be equivalent to v.
+        data(t+1, x, y, z) EQUALS v;
+    }
+};
+
+REGISTER_STENCIL(StreamStencil);
+
+// Reverse-time stencil.
+// In this test, data(t-1) depends on data(t).
+
+class TestReverseStencil : public StencilBase {
+
+protected:
+
+    // Indices & dimensions.
+    MAKE_STEP_INDEX(t);           // step in time dim.
+    MAKE_DOMAIN_INDEX(x);         // spatial dim.
+    MAKE_DOMAIN_INDEX(y);         // spatial dim.
+
+    // Vars.
+    MAKE_GRID(data, t, x, y);
+    
+public:
+
+    TestReverseStencil(StencilList& stencils) :
+        StencilBase("test_reverse", stencils) { }
+    virtual ~TestReverseStencil() { }
+
+    // Define equation to do simple test.
+    virtual void define() {
+
+        data(t-1, x, y) EQUALS data(t, x, y) + 5.0;
+    }
+};
+
+REGISTER_STENCIL(TestReverseStencil);

src/stencils/StreamStencil.hpp
@@ -1,66 +1,0 @@
-/*****************************************************************************
-
-YASK: Yet Another Stencil Kernel
-Copyright (c) 2014-2017, Intel Corporation
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to
-deal in the Software without restriction, including without limitation the
-rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-sell copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-* The above copyright notice and this permission notice shall be included in
-  all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-IN THE SOFTWARE.
-
-*****************************************************************************/
-
-// Implement a "stream-like" stencil that just reads and writes
-// with no spatial offsets.
-// The radius controls how many reads are done.
-
-#include "Soln.hpp"
-
-class StreamStencil : public StencilRadiusBase {
-
-protected:
-
-    // Indices & dimensions.
-    MAKE_STEP_INDEX(t);           // step in time dim.
-    MAKE_DOMAIN_INDEX(x);         // spatial dim.
-    MAKE_DOMAIN_INDEX(y);         // spatial dim.
-    MAKE_DOMAIN_INDEX(z);         // spatial dim.
-
-    // Vars.
-    MAKE_GRID(data, t, x, y, z); // time-varying 3D grid.
-    
-public:
-
-    StreamStencil(StencilList& stencils, int radius=8) :
-        StencilRadiusBase("stream", stencils, radius) { }
-    virtual ~StreamStencil() { }
-
-    // Define equation to read '_radius' values and write one.
-    virtual void define() {
-
-        GridValue v = constNum(1.0);
-
-        // Add '_radius' values from past time-steps to ensure no spatial locality.
-        for (int r = 0; r < _radius; r++) {
-            v += data(t-r, x, y, z);
-        }
-
-        // define the value at t+1 to be equivalent to v.
-        data(t+1, x, y, z) EQUALS v;
-    }
-};
-
-REGISTER_STENCIL(StreamStencil);

src/stencils/TestReverse.hpp
@@ -1,55 +1,0 @@
-/*****************************************************************************
-
-YASK: Yet Another Stencil Kernel
-Copyright (c) 2014-2017, Intel Corporation
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to
-deal in the Software without restriction, including without limitation the
-rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-sell copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-* The above copyright notice and this permission notice shall be included in
-  all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-IN THE SOFTWARE.
-
-*****************************************************************************/
-
-// Test reverse-time stencil.
-
-#include "Soln.hpp"
-
-class TestReverseStencil : public StencilBase {
-
-protected:
-
-    // Indices & dimensions.
-    MAKE_STEP_INDEX(t);           // step in time dim.
-    MAKE_DOMAIN_INDEX(x);         // spatial dim.
-    MAKE_DOMAIN_INDEX(y);         // spatial dim.
-
-    // Vars.
-    MAKE_GRID(data, t, x, y);
-    
-public:
-
-    TestReverseStencil(StencilList& stencils) :
-        StencilBase("test_reverse", stencils) { }
-    virtual ~TestReverseStencil() { }
-
-    // Define equation to do simple test.
-    virtual void define() {
-
-        data(t-1, x, y) EQUALS data(t, x, y) + 5.0;
-    }
-};
-
-REGISTER_STENCIL(TestReverseStencil);

src/kernel/tests/grid_test.cpp
@@ -24,6 +24,7 @@ IN THE SOFTWARE.
 *****************************************************************************/
 
 // Test the YASK grids.
+// This must be compiled with a kernel lib containing 'x', 'y', and 'z' dims.
 
 // enable assert().
 #define DEBUG