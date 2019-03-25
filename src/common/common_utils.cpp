15c62650afd2478032f7f164abb0495793568ff7
chuck.yount <cyount@ortce-hsw5.jf.intel.com>
2019-03-25 10:37:27 -0700

    Update version.

--- src/common/common_utils.cpp
+++ src/common/common_utils.cpp
@@ -43,7 +43,7 @@ namespace yask {
     // for numbers above 9 (at least up to 99).
 
     // Format: "major.minor.patch".
-    const string version = "2.19.01";
+    const string version = "2.19.02";
 
     string yask_get_version_string() {
         return version;
