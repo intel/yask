6b8ba04bc808c49c3328bb92d07efab460a1a4cb
Yount, Chuck <chuck.yount@intel.com>
2023-08-09 15:40:52 -0700

    Update API docs to doxygen 1.9.8

--- src/common/common_utils.cpp
+++ src/common/common_utils.cpp
@@ -44,7 +44,7 @@ namespace yask {
     // for numbers above 9 (at least up to 99).
 
     // Format: "major.minor.patch[-alpha|-beta]".
-    const string version = "4.04.03";
+    const string version = "4.04.04";
 
     string yask_get_version_string() {
         return version;
