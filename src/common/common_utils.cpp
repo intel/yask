040b6104cb7d45ae70df8fbcb83422c68f8a0fec
Yount, Chuck <chuck.yount@intel.com>
2024-04-25 15:57:01 -0700

    Add numa MPI pinning

--- src/common/common_utils.cpp
+++ src/common/common_utils.cpp
@@ -47,7 +47,7 @@ namespace yask {
     // https://semver.org/.
 
     // Format: "major.minor.patch[-alpha|-beta]".
-    const string version = "4.05.00";
+    const string version = "4.05.01";
 
     string yask_get_version_string() {
         return version;
